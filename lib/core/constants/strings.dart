43ca4fa155ec1a5803cd5cc77ae20d341eb2d8fd
AnneAlmd <annealmd@gmail.com>
2022-02-08 19:13:22 -0300

    :tada: initate the project

---
+++ lib/core/constants/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  // App
+  static const appTitle = 'App Name';
+
+  // HomeScreen
+  static const homeScreenTitle = 'Counter Screen';
+  static const homeScreenCenterText = 'Welcome Learner!';
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/constants/strings.dart';
+import 'core/themes/app_theme.dart';
+import 'logic/debug/app_bloc_observer.dart';
+import 'presentation/router/app_router.dart';
+
+void main() {
+  BlocOverrides.runZoned(
+    () {
+      // Use cubits...
+    },
+    blocObserver: AppBlocObserver(),
+  );
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Strings.appTitle,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRouter.counter,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+    );
+  }
+}
